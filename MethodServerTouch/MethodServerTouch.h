db719b4c20567673b37f8f2dc08f5b98aca1654c
Marcel Weiher <marcel@metaobject.com>
2014-07-06 17:10:33 +0200

    MethodServer as iOS8 framework

---
+++ MethodServerTouch/MethodServerTouch.h
@@ -0,0 +1,19 @@
+//
+//  MethodServerTouch.h
+//  MethodServerTouch
+//
+//  Created by Marcel Weiher on 7/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MethodServerTouch.
+FOUNDATION_EXPORT double MethodServerTouchVersionNumber;
+
+//! Project version string for MethodServerTouch.
+FOUNDATION_EXPORT const unsigned char MethodServerTouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MethodServerTouch/PublicHeader.h>
+
+
