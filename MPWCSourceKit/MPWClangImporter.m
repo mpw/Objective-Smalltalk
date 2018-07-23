e3721be4a7f0db9a735ea7204e4426c90c402226
Marcel Weiher <marcel@metaobject.com>
2018-07-24 00:14:58 +0200

    block args now correctly extracted

--- MPWCSourceKit/MPWClangImporter.m
+++ MPWCSourceKit/MPWClangImporter.m
@@ -60,7 +60,74 @@
     return [self cxstringToNSString:clang_getDeclObjCTypeEncoding(aCursor)];
 }
 
+-(void)handleBlockArgumentsAndReturnTypes:(CXCursor)classCursor
+{
+    printf("      block objc types %s\n",[[self typeAtCursor:classCursor] UTF8String]);
+    clang_visitChildrenWithBlock(classCursor,
+                                 ^ enum CXChildVisitResult (CXCursor blockArgCursor, CXCursor blockParent)
+                                 {
+                                     CXType argType=clang_getCursorType(blockArgCursor);
+                                     printf("      arg of block name=%s type=%s\n",
+                                            [[self nameAtCursor:blockArgCursor] UTF8String],
+                                            [[self cxstringToNSString:clang_getTypeSpelling(argType)] UTF8String]);
+                                     return CXChildVisit_Continue;
+                                 });
+}
 
+-(void)handleBlockArg:(CXCursor)argCursor
+{
+    CXType blockType=clang_getCursorType(argCursor);
+   printf("   block name: %s type: %s\n",
+           [[self nameAtCursor:argCursor] UTF8String],
+          [[self cxstringToNSString:clang_getTypeSpelling(blockType)] UTF8String]);
+    clang_visitChildrenWithBlock(argCursor,
+                                 ^ enum CXChildVisitResult (CXCursor blockCursor, CXCursor blockParent)
+         {
+             switch (blockCursor.kind) {
+                 case CXCursor_ParmDecl:
+                 {
+                     [self handleBlockArgumentsAndReturnTypes:blockCursor];
+                 }
+                 case CXCursor_UnexposedAttr:
+                     break;
+                 case CXCursor_ObjCClassRef:
+                 {
+                     CXType argType=clang_getCursorType(blockCursor);
+                     printf("     block arg classref name=%s type=%s\n",
+                            [[self nameAtCursor:blockCursor] UTF8String],
+                            [[self cxstringToNSString:clang_getTypeSpelling(argType)] UTF8String]);
+                     break;
+                 }
+                 default:
+                     printf("     unhandled in block: %d\n",blockCursor.kind);
+
+             }
+             return CXChildVisit_Continue;
+             
+         });
+    
+}
+
+-(void)handleArgumentsAndReturnTypes:(CXCursor)classCursor
+{
+    printf(" %s qualifiers: %x\n",[[self nameAtCursor:classCursor] UTF8String],clang_Cursor_getObjCDeclQualifiers(classCursor));
+    printf(" objc types %s\n",[[self typeAtCursor:classCursor] UTF8String]);
+    int numArgs=clang_Cursor_getNumArguments(classCursor);
+    printf(" %d arguments\n",numArgs);
+    for (int i=0;i<numArgs;i++) {
+        CXCursor argCursor=clang_Cursor_getArgument(classCursor,i);
+        CXType argType=clang_getCursorType(argCursor);
+        printf("  arg[%d] name=%s type=%s\n",i,
+               [[self nameAtCursor:argCursor] UTF8String],
+               [[self cxstringToNSString:clang_getTypeSpelling(argType)] UTF8String]
+               );
+        if (argType.kind==CXType_BlockPointer) {
+            [self handleBlockArg:argCursor];
+            
+        }
+    }
+
+}
 
 -(void)handleInterface:(CXCursor)cursor
 {
@@ -71,28 +138,7 @@
          {
              switch ( classCursor.kind) {
                  case CXCursor_ObjCInstanceMethodDecl:
-                     printf(" instance method %s qualifiers: %x\n",[[self nameAtCursor:classCursor] UTF8String],clang_Cursor_getObjCDeclQualifiers(classCursor));
-                     printf(" objc types %s\n",[[self typeAtCursor:classCursor] UTF8String]);
-                     int numArgs=clang_Cursor_getNumArguments(classCursor);
-                     printf(" %d arguments\n",numArgs);
-                     for (int i=0;i<numArgs;i++) {
-                         CXCursor argCursor=clang_Cursor_getArgument(classCursor,i);
-                         CXType argType=clang_getCursorType(argCursor);
-                         printf("  arg[%d] name=%s type=%s\n",i,
-                                [[self nameAtCursor:argCursor] UTF8String],
-                                [[self cxstringToNSString:clang_getTypeSpelling(argType)] UTF8String]
-                                );
-                         if (argType.kind==CXType_BlockPointer) {
-                             printf("   block\n");
-                             clang_visitChildrenWithBlock(argCursor,
-                                                          ^ enum CXChildVisitResult (CXCursor blockCursor, CXCursor blockParent)
-                                                          {
-                                                              printf("   unhandled in block: %d\n",blockCursor.kind);
-                                                              return CXChildVisit_Continue;
-
-                                                          });
-                         }
-                     }
+                     [self handleArgumentsAndReturnTypes:classCursor];
 
                      break;
                  case CXCursor_ObjCClassMethodDecl:
