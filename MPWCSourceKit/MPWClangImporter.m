425593c74813ea6e553fc0b35dedc125668b10cd
Marcel Weiher <marcel@metaobject.com>
2018-07-23 22:57:33 +0200

    analysing down to instance method argument names + types, need to iterate some more for block arg types

--- MPWCSourceKit/MPWClangImporter.m
+++ MPWCSourceKit/MPWClangImporter.m
@@ -14,6 +14,7 @@
     CXIndex clangIndex;
     CXTranslationUnit translationUnit;
     CXFile file;
+    
 }
 
 +(instancetype)importer
@@ -21,32 +22,113 @@
     return [[[self alloc] init] autorelease];
 }
 
--(void)initializeTranslationUnit
+-(void)initializeTranslationUnit:(nullable NSString*)filename
 {
-    const char *argv[]={ "", "", NULL};
-    int argc=0;
+    const char *argv[]={ "-v", "-v", NULL};
+    int argc=2;
     const char *mainFile = "/tmp/hi.m";
-    struct CXUnsavedFile unsaved[] = { {NULL, NULL, 0}};
+    NSString *syntheticMainfile=[NSString stringWithFormat:@"#define nullable \n#define nonnull \n#import <Foundation/Foundation.h>\n"];
+    struct CXUnsavedFile unsaved[] = {
+        {mainFile, [syntheticMainfile UTF8String], [syntheticMainfile length]},
+        {NULL, NULL, 0}};
+
     clangIndex = clang_createIndex(1, 1);
 
     
     
     
-    translationUnit = clang_parseTranslationUnit(clangIndex, mainFile, argv, argc, unsaved,
-                               0,
+    translationUnit =
+    clang_parseTranslationUnit(clangIndex, mainFile, argv, argc, unsaved,
+                               1,
                                clang_defaultEditingTranslationUnitOptions());
 }
 
+-(NSString*)cxstringToNSString:(CXString)cxname
+{
+    const char *cstrname=clang_getCString(cxname);
+    NSString *s=[NSString stringWithUTF8String:cstrname];
+    return s;
+}
+
+-(NSString*)nameAtCursor:(CXCursor)aCursor
+{
+    return [self cxstringToNSString:clang_getCursorSpelling(aCursor)];
+}
+
+-(NSString*)typeAtCursor:(CXCursor)aCursor
+{
+    return [self cxstringToNSString:clang_getDeclObjCTypeEncoding(aCursor)];
+}
+
+
+
+-(void)handleInterface:(CXCursor)cursor
+{
+    printf("interface %s\n",[[self nameAtCursor:cursor] UTF8String]);
+
+    clang_visitChildrenWithBlock(cursor,
+                                 ^ enum CXChildVisitResult (CXCursor classCursor, CXCursor parent)
+         {
+             switch ( classCursor.kind) {
+                 case CXCursor_ObjCInstanceMethodDecl:
+                     printf(" instance method %s qualifiers: %x\n",[[self nameAtCursor:classCursor] UTF8String],clang_Cursor_getObjCDeclQualifiers(classCursor));
+                     printf(" objc types %s\n",[[self typeAtCursor:classCursor] UTF8String]);
+                     int numArgs=clang_Cursor_getNumArguments(classCursor);
+                     printf(" %d arguments\n",numArgs);
+                     for (int i=0;i<numArgs;i++) {
+                         CXCursor argCursor=clang_Cursor_getArgument(classCursor,i);
+                         CXType argType=clang_getCursorType(argCursor);
+                         printf("  arg[%d] name=%s type=%s\n",i,
+                                [[self nameAtCursor:argCursor] UTF8String],
+                                [[self cxstringToNSString:clang_getTypeSpelling(argType)] UTF8String]);
+                     }
+
+                     break;
+                 case CXCursor_ObjCClassMethodDecl:
+                     printf(" class method %s\n",[[self nameAtCursor:classCursor] UTF8String]);
+                     break;
+                 case CXCursor_ObjCPropertyDecl:
+                     printf(" property decl %s\n",[[self nameAtCursor:classCursor] UTF8String]);
+                     break;
+                 default:
+                     printf(" unhandled in interface: %d\n",classCursor.kind);
+                     break;
+                     
+             }
+             return CXChildVisit_Continue;
+         });
+}
 
 -parseAFile
 {
-    [self initializeTranslationUnit];
+    [self initializeTranslationUnit:nil];
     clang_visitChildrenWithBlock(clang_getTranslationUnitCursor(translationUnit),
                                  ^ enum CXChildVisitResult (CXCursor cursor, CXCursor parent)
-                                 {
-                                     NSLog(@"found something");
-                                     return CXChildVisit_Continue;
-                                 });
+         {
+             switch ( cursor.kind) {
+                 case CXCursor_ObjCInterfaceDecl:
+                     [self handleInterface:cursor];
+                     break;
+                 case CXCursor_ObjCCategoryDecl:
+                     printf("category\n");
+                     break;
+                 case CXCursor_FunctionDecl:
+                     printf("function declaration\n");
+                     break;
+                 case CXCursor_EnumDecl:
+                     printf("enum\n");
+                     break;
+                 case CXCursor_VarDecl:
+                     printf("variable declaration\n");
+                     break;
+                 case CXCursor_TypedefDecl:
+                     printf("typedef\n");
+                     break;
+                 default:
+                     printf("unhandled: %d\n",cursor.kind);
+             }
+             return CXChildVisit_Continue;
+         });
     return nil;
 }
 
