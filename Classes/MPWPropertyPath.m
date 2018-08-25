1b8f12cc2cec069cb26c7f9276fbaf53750093e5
Marcel Weiher <marcel@metaobject.com>
2018-08-25 13:56:49 +0200

    matching property path definitions against paths works, including binding parameters

--- Classes/MPWPropertyPath.m
+++ Classes/MPWPropertyPath.m
@@ -36,9 +36,37 @@ CONVENIENCEANDINIT( propertyPath, WithPathString:(NSString*)path)
     return [(NSArray*)[[self.pathComponents collect] pathName] componentsJoinedByString:@"/"];
 }
 
--(NSDictionary*)bindingsForMatchedReference:(id <MPWReferencing>)ref;
+-(NSDictionary*)bindingsForMatchedReference:(id <MPWReferencing>)ref
 {
-    return nil;
+    NSMutableDictionary *result=[NSMutableDictionary dictionary];
+    NSArray *pathComponents=[ref relativePathComponents];
+
+    for (long i=0, max=MIN(pathComponents.count,self.pathComponents.count);i<max;i++) {
+        NSString *segment=pathComponents[i];
+        MPWPropertyPathComponent *matcher=self.pathComponents[i];
+        NSString *matcherName=matcher.name;
+        NSString *argName=matcher.parameter;
+
+        if ( matcherName ) {
+            if ( ![matcherName isEqualToString:segment]) {
+                return nil;
+            }
+        } else if ( argName) {
+            if ( matcher.isWildcard ) {
+                result[argName]=[pathComponents subarrayWithRange:NSMakeRange(i,pathComponents.count-i)];
+                break;
+            } else {
+                result[argName]=segment;
+            }
+        }
+    }
+    
+    return result;
+}
+
+-(NSDictionary*)bindingsForMatchedPath:(NSString*)path
+{
+    return [self bindingsForMatchedReference:[MPWGenericReference referenceWithPath:path]];
 }
 
 -(void)dealloc
@@ -63,10 +91,67 @@ CONVENIENCEANDINIT( propertyPath, WithPathString:(NSString*)path)
     
 }
 
++(void)testInitializeWithArguments
+{
+    MPWPropertyPath *pp=[self propertyPathWithPathString:@"hello/:arg1/world/:arg2"];
+    INTEXPECT([[pp pathComponents] count], 4, @"number of components")
+    IDEXPECT([pp.pathComponents[0] name], @"hello", @"name");
+    IDEXPECT([pp.pathComponents[1] parameter], @"arg1", @"arg1");
+    IDEXPECT([pp.pathComponents[2] name], @"world", @"name");
+    IDEXPECT([pp.pathComponents[3] parameter], @"arg2", @"arg2");
+    
+}
+
++(void)testInitializeWithWildcard
+{
+    MPWPropertyPath *pp=[self propertyPathWithPathString:@"hello/*:remainder"];
+    INTEXPECT([[pp pathComponents] count], 2, @"number of components")
+    IDEXPECT([pp.pathComponents[0] name], @"hello", @"name");
+    IDEXPECT([pp.pathComponents[1] parameter], @"remainder", @"arg1");
+    EXPECTTRUE([pp.pathComponents[1] isWildcard], @"wildcard");
+    
+}
+
++(void)testMatchAgainstConstantPath
+{
+    MPWPropertyPath *pp=[self propertyPathWithPathString:@"hello/world"];
+    NSDictionary *result=[pp bindingsForMatchedPath:@"hello/world"];
+    EXPECTNOTNIL(result,@"got a match");
+    INTEXPECT(result.count,0,@"no bound vars");
+    result=[pp bindingsForMatchedPath:@"h"];
+    EXPECTNIL(result,@"no match");
+    
+}
+
++(void)testMatchAgainstPathWithParameters
+{
+    MPWPropertyPath *pp=[self propertyPathWithPathString:@"hello/:arg1/world/:arg2"];
+    NSDictionary *result=[pp bindingsForMatchedPath:@"hello/this/world/cruel"];
+    EXPECTNOTNIL(result,@"got a match");
+    INTEXPECT(result.count,2,@"no bound vars");
+    IDEXPECT(result[@"arg1"],@"this",@"binding for arg1");
+    IDEXPECT(result[@"arg2"],@"cruel",@"binding for arg2");
+}
+
++(void)testMatchAgainstWildcard
+{
+    MPWPropertyPath *pp=[self propertyPathWithPathString:@"hello/:arg1/world/*:arg2"];
+    NSDictionary *result=[pp bindingsForMatchedPath:@"hello/this/world/cruel/remainder"];
+    EXPECTNOTNIL(result,@"got a match");
+    INTEXPECT(result.count,2,@"no bound vars");
+    IDEXPECT(result[@"arg2"],(@[ @"cruel", @"remainder"]),@"binding for arg2");
+
+}
+
 +testSelectors
 {
     return @[
-        @"testInitializeWithSingleConstantPath",
+             @"testInitializeWithSingleConstantPath",
+             @"testInitializeWithArguments",
+             @"testInitializeWithWildcard",
+             @"testMatchAgainstConstantPath",
+             @"testMatchAgainstPathWithParameters",
+             @"testMatchAgainstWildcard",
     ];
 }
 
