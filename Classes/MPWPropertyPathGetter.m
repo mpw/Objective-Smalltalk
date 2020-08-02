ab5044bdaf04a5a64fa82828ed5d5248b9114a57
Marcel Weiher <marcel@metaobject.com>
2020-08-02 23:56:23 +0200

    bug: conflicting definitions for -[NSDictionary at:] causes SiteBuilder/ClassBrowser to malfunction.  Workaround: use -objectForKey: in ClassBrowser.  (Should fix)

--- Classes/MPWPropertyPathGetter.m
+++ Classes/MPWPropertyPathGetter.m
@@ -24,13 +24,10 @@ CONVENIENCEANDINIT(getter, WithPropertyPathDefinitions:newPaths)
 
 -(id)evaluateFoundPropertyPath:(MPWPropertyPathDefinition*)def onTarget:target boundParams:(NSDictionary*)pathParams additionalParams:(NSArray*)param
 {
-    //            NSLog(@"matched: %@",def.propertyPath.pathComponents);
-    //            NSLog(@"matched args: %@",pathParams);
     MPWScriptedMethod *m=[def get];
     NSArray *params=[pathParams objectsForKeys:[def.propertyPath formalParameters] notFoundMarker:@""];
-    //            NSLog(@"array aprams: %@",params);
     params = [params arrayByAddingObject:param.lastObject];
-    id result=[m evaluateOnObject:target parameters:params];
+     id result=[m evaluateOnObject:target parameters:params];
     return result;
 }
 
@@ -38,16 +35,12 @@ CONVENIENCEANDINIT(getter, WithPropertyPathDefinitions:newPaths)
 -(id)evaluateOnObject:(id)target parameters:(NSArray *)parameters
 {
     id <MPWReferencing> ref=parameters.lastObject;
-//    NSLog(@"reference: %@",ref);
     for ( MPWPropertyPathDefinition *def in self.propertyPathDefs) {
-//        NSLog(@"try def: %@",def);
         NSDictionary *pathParams=[def.propertyPath bindingsForMatchedReference:ref];
         if (pathParams) {
-///            NSLog(@"match: %@",def);
             return [self evaluateFoundPropertyPath:def onTarget:target boundParams:pathParams additionalParams:parameters];
         }
     }
-//    NSLog(@"no match for %@",ref);
     [NSException raise:@"undefined" format:@"undefined path: %@ for object: %@",ref,target];
     return nil;
 }
