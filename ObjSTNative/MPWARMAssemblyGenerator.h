35d4cd174ed086319689572bd29549b10ee2cd40
Marcel Weiher <marcel@metaobject.com>
2020-12-04 08:42:15 +0100

    use cc with -dynamiclib instead of ld

---
+++ ObjSTNative/MPWARMAssemblyGenerator.h
@@ -0,0 +1,16 @@
+//
+//  MPWARMAssemblyGenerator.h
+//  ObjSTNative
+//
+//  Created by Marcel Weiher on 04.12.20.
+//
+
+#import <MPWFoundation/MPWFoundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MPWARMAssemblyGenerator : MPWFilter
+
+@end
+
+NS_ASSUME_NONNULL_END
