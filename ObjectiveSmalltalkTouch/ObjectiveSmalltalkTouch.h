61e37411882a04ad380999d038937f7cd57b053e
Marcel Weiher <marcel@metaobject.com>
2021-01-20 13:44:18 +0100

    added iOS version of ObjectiveSmalltalkUI framework -> ObjectiveSmalltalkTouchUI project

--- ObjectiveSmalltalkTouch/ObjectiveSmalltalkTouch.h
+++ ObjectiveSmalltalkTouch/ObjectiveSmalltalkTouch.h
@@ -14,6 +14,7 @@ FOUNDATION_EXPORT double ObjectiveSmalltalkTouchVersionNumber;
 //! Project version string for ObjectiveSmalltalkTouch.
 FOUNDATION_EXPORT const unsigned char ObjectiveSmalltalkTouchVersionString[];
 
+
 // In this header, you should import all the public headers of your framework using statements like #import <ObjectiveSmalltalkTouch/PublicHeader.h>
 
 
