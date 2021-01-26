f7f1811b3e5d06e50abc1a1278cf35cd4c6d9893
Marcel Weiher <marcel@metaobject.com>
2021-01-26 10:19:42 +0100

    font store for UIKit

---
+++ ObjectiveSmalltalkTouchUI/MPWFontStore.h
@@ -0,0 +1,16 @@
+//
+//  MPWFontStore.h
+//  ObjectiveSmalltalkTouchUI
+//
+//  Created by Marcel Weiher on 26.01.21.
+//
+
+#import <MPWFoundation/MPWFoundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MPWFontStore : MPWAbstractStore
+
+@end
+
+NS_ASSUME_NONNULL_END
