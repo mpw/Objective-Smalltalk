7686dc5540ec09ce70e3007b59bb28fa5dc6f3db
Marcel Weiher <marcel@metaobject.com>
2021-04-23 16:56:02 +0200

    |= hooked up to SimpleDataflowConstraint

--- Classes/MPWDataflowConstraintExpression.m
+++ Classes/MPWDataflowConstraintExpression.m
@@ -7,12 +7,61 @@
 //
 
 #import "MPWDataflowConstraintExpression.h"
+#import "STSimpleDataflowConstraint.h"
+#import "MPWIdentifierExpression.h"
 
 @implementation MPWDataflowConstraintExpression
 
+-(MPWBinding*)bindingForIdentifier:(MPWIdentifierExpression*)expr context:aContext
+{
+    return [[expr identifier] bindingWithContext:aContext];
+}
+
 -(id)evaluateIn:(id)aContext
 {
-    @throw [NSException exceptionWithName:@"constraint" reason:@"Unidirectional constraints not implemented" userInfo:nil];
+    if ( [rhs isKindOfClass:[MPWIdentifierExpression class]] &&
+        [lhs isKindOfClass:[MPWIdentifierExpression class]] ) {
+        MPWBinding *lhb=[self bindingForIdentifier:lhs context:aContext];
+        MPWBinding *rhb=[self bindingForIdentifier:(MPWIdentifierExpression*)rhs context:aContext];
+        return [STSimpleDataflowConstraint constraintWithSource:rhb target:lhb];
+    } else {
+        [NSException raise:@"typecheck" format:@"Both LHS and RHS of |= dataflow constraint must be identifiers"];
+    }
+    return nil;
+    
+}
+
+@end
+
+
+
+@implementation MPWDataflowConstraintExpression(testing)
+
+
++(void)testExpressionReturnsDataflowConstraint
+{
+    STCompiler *compiler=[STCompiler compiler];
+    [compiler evaluateScriptString:@"a ← 3. b ← 5."];
+    STSimpleDataflowConstraint *s=[compiler evaluateScriptString:@"a |= b."];
+    EXPECTTRUE([s isKindOfClass:[STSimpleDataflowConstraint class]],@"should return a dataflow constraint");
+}
+
++(void)testConstraintIsUsable
+{
+    STCompiler *compiler=[STCompiler compiler];
+    [compiler evaluateScriptString:@"a ← 3. b ← 5."];
+    STSimpleDataflowConstraint *s=[compiler evaluateScriptString:@"a |= b."];
+    [s update];
+    IDEXPECT( [compiler evaluateScriptString:@"a"],@5, @"did update");
+}
+
++testSelectors
+{
+    return @[
+        @"testExpressionReturnsDataflowConstraint",
+        @"testConstraintIsUsable",
+        @"testConstraintIsUsable",
+    ];
 }
 
 @end
