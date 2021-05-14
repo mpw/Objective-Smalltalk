138590fb300b6fb976f0ce0ccf0eaa19cdeb89e7
Marcel Weiher <marcel@metaobject.com>
2021-05-14 20:33:21 +0200

    make shell with active runloop work on GNUstep (raspi); raspish now starts up that way

--- raspi/stsh_main.m
+++ raspi/stsh_main.m
@@ -21,7 +21,7 @@ int main (int argc, const char *argv[])
     MPWStsh *stsh=[[[MPWStsh alloc] initWithArgs:args] autorelease];
     [[stsh evaluator] evaluateScriptString:@"scheme:gpio := MPWBCMStore store."];
     [[stsh evaluator] evaluateScriptString:@"object Blinker : #MPWFixedValueSource{ #values: #(0,1) }"];
-    [stsh run];
+    [stsh runInRunLoop];
     exit(0);       // insure the process exit status is 0
     return 0;      // ...and make main fit the ANSI spec.
 }
