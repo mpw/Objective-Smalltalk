89feb9371604756a599d6f89ef40a42319f31862
Marcel Weiher <marcel@metaobject.com>
2021-06-27 19:36:55 +0200

    enough of ARM asssembly generator to do simplistic hello world

--- ObjSTNative/MPWARMAssemblyGenerator.m
+++ ObjSTNative/MPWARMAssemblyGenerator.m
@@ -7,6 +7,8 @@
 
 #import "MPWARMAssemblyGenerator.h"
 
+#define SP   -1
+
 @implementation MPWARMAssemblyGenerator
 
 -(void)label:(NSString*)label
@@ -30,16 +32,55 @@
     [self directive:@"align" arg:@(alignment)];
 }
 
+-(void)outputRegister:(int)regno
+{
+    if ( regno == SP ) {
+        [self printf:@"SP"];
+    } else {
+        [self printf:@"X%d",regno];
+    }
+}
+
+
+-(void)outputInstruction:(NSString*)instruction regNo:(int)regNo value:(long)value
+{
+    [self printFormat:@"\t%@\t",instruction];
+    [self outputRegister:regNo];
+    [self printf:@", #%ld\n",value];
+}
 
 -(void)mov:(int)regNo value:(long)value
 {
-    [self printf:@"\tmov\tX%d, #%ld\n",regNo,value];
+    [self outputInstruction:@"mov" regNo:regNo value:value];
+}
+
+-(void)svc:(int)vector
+{
+    [self printFormat:@"\tsvc\t#%ld\n",vector];
 }
 
 -(void)ret
 {
     [self printf:@"\tret\n"];
 }
+
+-(void)bl:(NSString*)address
+{
+    [self printFormat:@"\tbl\t%@\n",address];
+}
+
+-(void)adr:(int)regNo address:(NSString*)address
+{
+    [self printFormat:@"\tadr\t"];
+    [self outputRegister:regNo];
+    [self printFormat:@", %@\n",address];
+}
+
+-(void)asciiz:(NSString*)str
+{
+    [self printFormat:@"\t.asciz\t\"%@\"\n",str];
+}
+
 
 @end
 
@@ -62,11 +103,27 @@
     IDEXPECT( [[g target] stringValue], @"\tmov\tX1, #1234\n",@"mov");
 }
 
++(void)testGenerateSPMove
+{
+    MPWARMAssemblyGenerator *g=[self stream];
+    [g mov:SP value:34];
+    IDEXPECT( [[g target] stringValue], @"\tmov\tSP, #34\n",@"mov with SP");
+}
+
++(void)testGenerateASCIZ
+{
+    MPWARMAssemblyGenerator *g=[self stream];
+    [g asciiz:@"Hello World"];
+    IDEXPECT( [[g target] stringValue], @"\t.asciz\t\"Hello World\"\n",@"string");
+}
+
 +(NSArray*)testSelectors
 {
    return @[
        @"testGenerateLabel",
        @"testGenerateMove",
+       @"testGenerateSPMove",
+       @"testGenerateASCIZ",
 			];
 }
 
