c5f2f4b22a1ba4ec12041f2532be68387cda5bd6
Marcel Weiher <marcel@metaobject.com>
2021-06-27 16:03:40 +0200

    start of ARM assembly generator; disable LLVM codegen + tests because I don't have a full LLVM install (and so tests depending on it fail)

--- ObjSTNative/MPWARMAssemblyGenerator.h
+++ ObjSTNative/MPWARMAssemblyGenerator.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MPWARMAssemblyGenerator : MPWFilter
+@interface MPWARMAssemblyGenerator : MPWByteStream
 
 @end
 
--- ObjSTNative/MPWARMAssemblyGenerator.m
+++ ObjSTNative/MPWARMAssemblyGenerator.m
@@ -9,6 +9,38 @@
 
 @implementation MPWARMAssemblyGenerator
 
+-(void)label:(NSString*)label
+{
+    [self outputString:label];
+    [self printf:@":\n"];
+}
+
+-(void)directive:(NSString*)directive arg:arg
+{
+    [self printFormat:@".%@\t%@\n",directive,[arg stringValue]];
+}
+
+-(void)global:(NSString*)label
+{
+    [self directive:@"global" arg:label];
+}
+
+-(void)align:(int)alignment
+{
+    [self directive:@"align" arg:@(alignment)];
+}
+
+
+-(void)mov:(int)regNo value:(long)value
+{
+    [self printf:@"\tmov\tX%d, #%ld\n",regNo,value];
+}
+
+-(void)ret
+{
+    [self printf:@"\tret\n"];
+}
+
 @end
 
 
@@ -16,15 +48,25 @@
 
 @implementation MPWARMAssemblyGenerator(testing) 
 
-+(void)someTest
++(void)testGenerateLabel
 {
-	EXPECTTRUE(false, @"implemented");
+    MPWARMAssemblyGenerator *g=[self stream];
+    [g label:@"hi"];
+    IDEXPECT( [[g target] stringValue], @"hi:\n",@"label");
+}
+
++(void)testGenerateMove
+{
+    MPWARMAssemblyGenerator *g=[self stream];
+    [g mov:1 value:1234];
+    IDEXPECT( [[g target] stringValue], @"\tmov\tX1, #1234\n",@"mov");
 }
 
 +(NSArray*)testSelectors
 {
    return @[
-//			@"someTest",
+       @"testGenerateLabel",
+       @"testGenerateMove",
 			];
 }
 
