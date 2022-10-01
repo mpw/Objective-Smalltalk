c3138d90ae020b079f8c607adf09af03f39d7e1c
Marcel Weiher <marcel@metaobject.com>
2022-10-01 09:33:58 +0200

    Revert "start reading Objective-C data; since we now have multiple segments, need a separate segment abstraction"
    This reverts commit a86fd1e43273469a6fc269a11995e797cbd8d246.

--- ObjSTNative/test-compiles/class-with-method.s
+++ ObjSTNative/test-compiles/class-with-method.s
@@ -1,107 +1,62 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 12, 0	sdk_version 12, 3
-	.p2align	2                               ; -- Begin function -[Hi components:splitInto:]
-"-[Hi components:splitInto:]":          ; @"\01-[Hi components:splitInto:]"
-	.cfi_startproc
-; %bb.0:
-	sub	sp, sp, #48
-	stp	x29, x30, [sp, #32]             ; 16-byte Folded Spill
-	add	x29, sp, #32
-	.cfi_def_cfa w29, 16
-	.cfi_offset w30, -8
-	.cfi_offset w29, -16
-	mov	x8, x1
-                                        ; implicit-def: $x1
-	stur	x0, [x29, #-8]
-	str	x8, [sp, #16]
-	str	x2, [sp, #8]
-	str	x3, [sp]
-	ldr	x0, [sp, #8]
-	ldr	x2, [sp]
-	bl	"_objc_msgSend$componentsSeparatedByString:"
-	ldp	x29, x30, [sp, #32]             ; 16-byte Folded Reload
-	add	sp, sp, #48
-	ret
-	.cfi_endproc
-                                        ; -- End function
-	.section	__TEXT,__objc_classname,cstring_literals
-l_OBJC_CLASS_NAME_:                     ; @OBJC_CLASS_NAME_
-	.asciz	"Hi"
+; ModuleID = 'class-with-method.m'
+target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx10.8.0"
 
-	.section	__DATA,__objc_const
-	.p2align	3                               ; @"_OBJC_METACLASS_RO_$_Hi"
-__OBJC_METACLASS_RO_$_Hi:
-	.long	1                               ; 0x1
-	.long	40                              ; 0x28
-	.long	40                              ; 0x28
-	.space	4
-	.quad	0
-	.quad	l_OBJC_CLASS_NAME_
-	.quad	0
-	.quad	0
-	.quad	0
-	.quad	0
-	.quad	0
+%0 = type opaque
+%1 = type opaque
+%2 = type opaque
+%struct._objc_cache = type opaque
+%struct._class_t = type { %struct._class_t*, %struct._class_t*, %struct._objc_cache*, i8* (i8*, i8*)**, %struct._class_ro_t* }
+%struct._class_ro_t = type { i32, i32, i32, i8*, i8*, %struct.__method_list_t*, %struct._objc_protocol_list*, %struct._ivar_list_t*, i8*, %struct._prop_list_t* }
+%struct.__method_list_t = type { i32, i32, [0 x %struct._objc_method] }
+%struct._objc_method = type { i8*, i8*, i8* }
+%struct._objc_protocol_list = type { i64, [0 x %struct._protocol_t*] }
+%struct._protocol_t = type { i8*, i8*, %struct._objc_protocol_list*, %struct.__method_list_t*, %struct.__method_list_t*, %struct.__method_list_t*, %struct.__method_list_t*, %struct._prop_list_t*, i32, i32, i8** }
+%struct._prop_list_t = type { i32, i32, [0 x %struct._prop_t] }
+%struct._prop_t = type { i8*, i8* }
+%struct._ivar_list_t = type { i32, i32, [0 x %struct._ivar_t] }
+%struct._ivar_t = type { i64*, i8*, i8*, i32, i32 }
 
-	.section	__DATA,__objc_data
-	.globl	_OBJC_METACLASS_$_Hi            ; @"OBJC_METACLASS_$_Hi"
-	.p2align	3
-_OBJC_METACLASS_$_Hi:
-	.quad	_OBJC_METACLASS_$_NSObject
-	.quad	_OBJC_METACLASS_$_NSObject
-	.quad	__objc_empty_cache
-	.quad	0
-	.quad	__OBJC_METACLASS_RO_$_Hi
+@"\01L_OBJC_METH_VAR_NAME_" = internal global [29 x i8] c"componentsSeparatedByString:\00", section "__TEXT,__objc_methname,cstring_literals", align 1
+@"\01L_OBJC_SELECTOR_REFERENCES_" = internal global i8* getelementptr inbounds ([29 x i8]* @"\01L_OBJC_METH_VAR_NAME_", i32 0, i32 0), section "__DATA, __objc_selrefs, literal_pointers, no_dead_strip"
+@_objc_empty_cache = external global %struct._objc_cache
+@_objc_empty_vtable = external global i8* (i8*, i8*)*
+@"OBJC_METACLASS_$_NSObject" = external global %struct._class_t
+@"\01L_OBJC_CLASS_NAME_" = internal global [3 x i8] c"Hi\00", section "__TEXT,__objc_classname,cstring_literals", align 1
+@"\01l_OBJC_METACLASS_RO_$_Hi" = internal global %struct._class_ro_t { i32 1, i32 40, i32 40, i8* null, i8* getelementptr inbounds ([3 x i8]* @"\01L_OBJC_CLASS_NAME_", i32 0, i32 0), %struct.__method_list_t* null, %struct._objc_protocol_list* null, %struct._ivar_list_t* null, i8* null, %struct._prop_list_t* null }, section "__DATA, __objc_const", align 8
+@"OBJC_METACLASS_$_Hi" = global %struct._class_t { %struct._class_t* @"OBJC_METACLASS_$_NSObject", %struct._class_t* @"OBJC_METACLASS_$_NSObject", %struct._objc_cache* @_objc_empty_cache, i8* (i8*, i8*)** @_objc_empty_vtable, %struct._class_ro_t* @"\01l_OBJC_METACLASS_RO_$_Hi" }, section "__DATA, __objc_data", align 8
+@"OBJC_CLASS_$_NSObject" = external global %struct._class_t
+@"\01L_OBJC_METH_VAR_NAME_1" = internal global [22 x i8] c"components:splitInto:\00", section "__TEXT,__objc_methname,cstring_literals", align 1
+@"\01L_OBJC_METH_VAR_TYPE_" = internal global [14 x i8] c"@32@0:8@16@24\00", section "__TEXT,__objc_methtype,cstring_literals", align 1
+@"\01l_OBJC_$_INSTANCE_METHODS_Hi" = internal global { i32, i32, [1 x %struct._objc_method] } { i32 24, i32 1, [1 x %struct._objc_method] [%struct._objc_method { i8* getelementptr inbounds ([22 x i8]* @"\01L_OBJC_METH_VAR_NAME_1", i32 0, i32 0), i8* getelementptr inbounds ([14 x i8]* @"\01L_OBJC_METH_VAR_TYPE_", i32 0, i32 0), i8* bitcast (%0* (%1*, i8*, %2*, %2*)* @"\01-[Hi components:splitInto:]" to i8*) }] }, section "__DATA, __objc_const", align 8
+@"\01l_OBJC_CLASS_RO_$_Hi" = internal global %struct._class_ro_t { i32 0, i32 8, i32 8, i8* null, i8* getelementptr inbounds ([3 x i8]* @"\01L_OBJC_CLASS_NAME_", i32 0, i32 0), %struct.__method_list_t* bitcast ({ i32, i32, [1 x %struct._objc_method] }* @"\01l_OBJC_$_INSTANCE_METHODS_Hi" to %struct.__method_list_t*), %struct._objc_protocol_list* null, %struct._ivar_list_t* null, i8* null, %struct._prop_list_t* null }, section "__DATA, __objc_const", align 8
+@"OBJC_CLASS_$_Hi" = global %struct._class_t { %struct._class_t* @"OBJC_METACLASS_$_Hi", %struct._class_t* @"OBJC_CLASS_$_NSObject", %struct._objc_cache* @_objc_empty_cache, i8* (i8*, i8*)** @_objc_empty_vtable, %struct._class_ro_t* @"\01l_OBJC_CLASS_RO_$_Hi" }, section "__DATA, __objc_data", align 8
+@"\01L_OBJC_LABEL_CLASS_$" = internal global [1 x i8*] [i8* bitcast (%struct._class_t* @"OBJC_CLASS_$_Hi" to i8*)], section "__DATA, __objc_classlist, regular, no_dead_strip", align 8
+@llvm.used = appending global [7 x i8*] [i8* getelementptr inbounds ([29 x i8]* @"\01L_OBJC_METH_VAR_NAME_", i32 0, i32 0), i8* bitcast (i8** @"\01L_OBJC_SELECTOR_REFERENCES_" to i8*), i8* getelementptr inbounds ([3 x i8]* @"\01L_OBJC_CLASS_NAME_", i32 0, i32 0), i8* getelementptr inbounds ([22 x i8]* @"\01L_OBJC_METH_VAR_NAME_1", i32 0, i32 0), i8* getelementptr inbounds ([14 x i8]* @"\01L_OBJC_METH_VAR_TYPE_", i32 0, i32 0), i8* bitcast ({ i32, i32, [1 x %struct._objc_method] }* @"\01l_OBJC_$_INSTANCE_METHODS_Hi" to i8*), i8* bitcast ([1 x i8*]* @"\01L_OBJC_LABEL_CLASS_$" to i8*)], section "llvm.metadata"
 
-	.section	__TEXT,__objc_methname,cstring_literals
-l_OBJC_METH_VAR_NAME_:                  ; @OBJC_METH_VAR_NAME_
-	.asciz	"components:splitInto:"
+define internal %0* @"\01-[Hi components:splitInto:]"(%1* %self, i8* %_cmd, %2* %s, %2* %delimiter) uwtable ssp {
+  %1 = alloca %1*, align 8
+  %2 = alloca i8*, align 8
+  %3 = alloca %2*, align 8
+  %4 = alloca %2*, align 8
+  store %1* %self, %1** %1, align 8
+  store i8* %_cmd, i8** %2, align 8
+  store %2* %s, %2** %3, align 8
+  store %2* %delimiter, %2** %4, align 8
+  %5 = load %2** %3, align 8
+  %6 = load %2** %4, align 8
+  %7 = load i8** @"\01L_OBJC_SELECTOR_REFERENCES_", !invariant.load !4
+  %8 = bitcast %2* %5 to i8*
+  %9 = call %0* bitcast (i8* (i8*, i8*, ...)* @objc_msgSend to %0* (i8*, i8*, %2*)*)(i8* %8, i8* %7, %2* %6)
+  ret %0* %9
+}
 
-	.section	__TEXT,__objc_methtype,cstring_literals
-l_OBJC_METH_VAR_TYPE_:                  ; @OBJC_METH_VAR_TYPE_
-	.asciz	"@32@0:8@16@24"
+declare i8* @objc_msgSend(i8*, i8*, ...) nonlazybind
 
-	.section	__DATA,__objc_const
-	.p2align	3                               ; @"_OBJC_$_INSTANCE_METHODS_Hi"
-__OBJC_$_INSTANCE_METHODS_Hi:
-	.long	24                              ; 0x18
-	.long	1                               ; 0x1
-	.quad	l_OBJC_METH_VAR_NAME_
-	.quad	l_OBJC_METH_VAR_TYPE_
-	.quad	"-[Hi components:splitInto:]"
+!llvm.module.flags = !{!0, !1, !2, !3}
 
-	.p2align	3                               ; @"_OBJC_CLASS_RO_$_Hi"
-__OBJC_CLASS_RO_$_Hi:
-	.long	0                               ; 0x0
-	.long	8                               ; 0x8
-	.long	8                               ; 0x8
-	.space	4
-	.quad	0
-	.quad	l_OBJC_CLASS_NAME_
-	.quad	__OBJC_$_INSTANCE_METHODS_Hi
-	.quad	0
-	.quad	0
-	.quad	0
-	.quad	0
-
-	.section	__DATA,__objc_data
-	.globl	_OBJC_CLASS_$_Hi                ; @"OBJC_CLASS_$_Hi"
-	.p2align	3
-_OBJC_CLASS_$_Hi:
-	.quad	_OBJC_METACLASS_$_Hi
-	.quad	_OBJC_CLASS_$_NSObject
-	.quad	__objc_empty_cache
-	.quad	0
-	.quad	__OBJC_CLASS_RO_$_Hi
-
-	.section	__DATA,__objc_classlist,regular,no_dead_strip
-	.p2align	3                               ; @"OBJC_LABEL_CLASS_$"
-l_OBJC_LABEL_CLASS_$:
-	.quad	_OBJC_CLASS_$_Hi
-
-	.section	__DATA,__objc_imageinfo,regular,no_dead_strip
-L_OBJC_IMAGE_INFO:
-	.long	0
-	.long	64
-
-.subsections_via_symbols
+!0 = metadata !{i32 1, metadata !"Objective-C Version", i32 2}
+!1 = metadata !{i32 1, metadata !"Objective-C Image Info Version", i32 0}
+!2 = metadata !{i32 1, metadata !"Objective-C Image Info Section", metadata !"__DATA, __objc_imageinfo, regular, no_dead_strip"}
+!3 = metadata !{i32 4, metadata !"Objective-C Garbage Collection", i32 0}
+!4 = metadata !{}
