f54d96bf83c6cc9f7d9710363730388e30aed913
Marcel Weiher <marcel@metaobject.com>
2023-05-27 17:57:27 +0200

    initial exploration of Python integration

---
+++ PyObjS/PyObjS.h
@@ -0,0 +1,18 @@
+//
+//  PyObjS.h
+//  PyObjS
+//
+//  Created by Marcel Weiher on 27.05.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PyObjS.
+FOUNDATION_EXPORT double PyObjSVersionNumber;
+
+//! Project version string for PyObjS.
+FOUNDATION_EXPORT const unsigned char PyObjSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PyObjS/PublicHeader.h>
+
+
---
+++ PyObjS/STYtb_dlp.h
@@ -0,0 +1,16 @@
+//
+//  STYtb_dlp.h
+//  PyObjS
+//
+//  Created by Marcel Weiher on 27.05.23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface STYtb_dlp : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
