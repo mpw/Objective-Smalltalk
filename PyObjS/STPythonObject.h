d896965466980e6ac3f078fc4a3ad1e9a786d5de
Marcel Weiher <marcel@metaobject.com>
2023-05-27 21:13:52 +0200

    with STPythonObject and some conversion utilities, things get a lot more compact

---
+++ PyObjS/STPythonObject.h
@@ -0,0 +1,30 @@
+//
+//  STPythonObject.h
+//  PyObjS
+//
+//  Created by Marcel Weiher on 27.05.23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+//typedef struct Object PyObject;
+
+@interface STPythonObject : NSObject
+
++(instancetype)pyString:(NSString*)s;
++(instancetype)pyObject:(void*)pyObjectIn;
+-(instancetype)initWithPyObject:(void*)pyObjectIn;
+-(instancetype)at:(NSString*)s;
+-(void*)pythonObject;
+-(instancetype)call:arg;
+-objectForKeyedSubscript:key;
+
+@end
+
+@interface NSObject(asPythonObject)
+-(STPythonObject*)asPythonObject;
+@end
+
+NS_ASSUME_NONNULL_END
