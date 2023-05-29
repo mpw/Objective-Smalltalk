becf8b35492195f02dd7ffbcace00498da184c65
Marcel Weiher <marcel@metaobject.com>
2023-05-29 08:56:41 +0200

    started ELF compilation project with ELF reader

---
+++ ObjSTNative/MPWELFReader.h
@@ -0,0 +1,16 @@
+//
+//  MPWELFReader.h
+//  ObjSTNative
+//
+//  Created by Marcel Weiher on 29.05.23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MPWELFReader : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
