81ab50181d8a0a478902bc0b2317b393f62fdea5
Marcel Weiher <marcel@metaobject.com>
2023-09-25 16:08:59 +0200

    STSlider

---
+++ ObjectiveSmalltalkUI/STSlider.h
@@ -0,0 +1,22 @@
+//
+//  STSlider.h
+//  ObjectiveSmalltalkUI
+//
+//  Created by Marcel Weiher on 25.09.23.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <MPWFoundation/MPWFoundation.h>
+#import <ObjectiveSmalltalkUI/ModelDidChangeNotification.h>
+
+@protocol ModelDidChange;
+NS_ASSUME_NONNULL_BEGIN
+
+@interface STSlider : NSSlider <ModelDidChange,ValidationDidChange>
+
+@property (nonatomic, strong)  MPWBinding *ref;
+@property (nonatomic, strong)  MPWBinding *enabledRef;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjectiveSmalltalkUI/STSlider.m
@@ -0,0 +1,64 @@
+//
+//  STSlider.m
+//  ObjectiveSmalltalkUI
+//
+//  Created by Marcel Weiher on 25.09.23.
+//
+
+#import "STSlider.h"
+
+@implementation STSlider
+
+-(instancetype)initWithFrame:(NSRect)frameRect
+{
+    self=[super initWithFrame:frameRect];
+    [self installProtocolNotifications];
+    return self;
+}
+
+-(BOOL)matchesRef:(id <MPWReferencing>)ref
+{
+    return YES;
+}
+
+-(void)modelDidChange:(NSNotification*)notification
+{
+    [self updateFromRef];
+}
+
+-(void)validationDidChange:(NSNotification*)notification
+{
+    //    NSLog(@"text field validation changed: %@ from %@",notification,self.enabledRef);
+    if ( self.enabledRef ) {
+        self.enabled = [self.enabledRef.value boolValue];
+        //        NSLog(@"enabled now: %d",self.enabled);
+    }
+}
+
+-(void)updateFromRef
+{
+    if ( self.ref ) {
+        self.doubleValue = [self.ref.value doubleValue];
+    }
+    if ( self.enabledRef ) {
+        self.enabled = [self.enabledRef.value boolValue];
+    }
+}
+
+
+-(void)updateToRef
+{
+    if (self.ref) {
+        self.ref.value = @(self.doubleValue);
+    }
+}
+
+-(void)setBinding:(MPWBinding*)newBinding
+{
+    self.ref = newBinding;
+    self.target = self;
+    self.action = @selector(updateToRef);
+}
+
+
+@end
