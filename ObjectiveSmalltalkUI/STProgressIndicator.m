2f2e985337e3215b029eba392324b82d34981980
Marcel Weiher <marcel@metaobject.com>
2023-09-26 10:21:25 +0200

    STProgressIndicator

---
+++ ObjectiveSmalltalkUI/STProgressIndicator.m
@@ -0,0 +1,54 @@
+//
+//  STProgressIndicator.m
+//  ObjectiveSmalltalkUI
+//
+//  Created by Marcel Weiher on 25.09.23.
+//
+
+#import "STProgressIndicator.h"
+
+@implementation STProgressIndicator
+
+-(instancetype)initWithFrame:(NSRect)frameRect
+{
+    self=[super initWithFrame:frameRect];
+    [self installProtocolNotifications];
+    return self;
+}
+
+-(BOOL)matchesRef:(id <MPWReferencing>)ref
+{
+    return YES;
+}
+
+-(void)modelDidChange:(NSNotification*)notification
+{
+    [self updateFromRef];
+}
+
+-(void)validationDidChange:(NSNotification*)notification
+{
+    if ( self.enabledRef ) {
+//        self.enabled = [self.enabledRef.value boolValue];
+    }
+}
+
+-(void)updateFromRef
+{
+    if ( self.ref ) {
+        self.doubleValue = [self.ref.value doubleValue];
+    }
+    if ( self.minRef ) {
+        self.minValue = [self.minRef.value doubleValue];
+    }
+    if ( self.maxRef ) {
+        self.maxValue = [self.maxRef.value doubleValue];
+    }
+    if ( self.enabledRef ) {
+//        self.enabled = [self.enabledRef.value boolValue];
+    }
+}
+
+
+
+@end
