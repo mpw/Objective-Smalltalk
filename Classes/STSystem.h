d2d5d592f610fa825dfc92c4e2b535883df1b27a
Marcel Weiher <marcel@metaobject.com>
2024-01-02 11:28:16 +0100

    added STSystem base class for systems and 'system' syntax that creates subclasses of STSystem by default

---
+++ Classes/STSystem.h
@@ -0,0 +1,16 @@
+//
+//  STSystem.h
+//  ObjectiveSmalltalk
+//
+//  Created by Marcel Weiher on 02.01.24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface STSystem : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
