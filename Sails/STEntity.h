5d683dd67c283bc528892d0125025d6b579c9e28
Marcel Weiher <marcel@metaobject.com>
2024-03-05 09:59:44 +0100

    moved id-handling to framework

---
+++ Sails/STEntity.h
@@ -0,0 +1,16 @@
+//
+//  STEntity.h
+//  Sails
+//
+//  Created by Marcel Weiher on 05.03.24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface STEntity : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
