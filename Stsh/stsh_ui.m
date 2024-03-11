ce3a12a354e0adcc6be30499b235b108eea013d7
Marcel Weiher <marcel@metaobject.com>
2024-03-11 16:33:50 +0100

    misc formatting/stylistic improvements, mostly getting rid of unneeded #

--- Stsh/stsh_ui.m
+++ Stsh/stsh_ui.m
@@ -66,7 +66,7 @@ int main (int argc, const char *argv[])
 
 -main:args
 {
-    NSLog(@"window main:");
+//    NSLog(@"window main:");
     [[CLIApp sharedApplication] runFromCLI:self];
     return @(0);
 }
