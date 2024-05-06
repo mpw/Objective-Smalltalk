f694e64741146e6539919ac495e9e3b7bca9f013
Marcel Weiher <marcel@metaobject.com>
2024-05-06 10:46:39 +0200

    introduced MPWObjectFileWriter, common superclass for ELFWriter and MachOWriter (for at leas the common string table handling)

---
+++ ObjSTNative/MPWObjectFileWriter.h
@@ -0,0 +1,16 @@
+//
+//  MPWObjectFileWriter.h
+//  ObjSTNative
+//
+//  Created by Marcel Weiher on 06.05.24.
+//
+
+#import <MPWFoundation/MPWFoundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MPWObjectFileWriter : MPWByteStream
+
+@end
+
+NS_ASSUME_NONNULL_END
