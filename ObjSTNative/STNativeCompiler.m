ad9e80819183902fd7077d32edff500705a714d6
Marcel Weiher <marcel@metaobject.com>
2024-05-11 14:22:26 +0200

    started to split out Mach-O specific tests from STNativeCompiler

--- ObjSTNative/STNativeCompiler.m
+++ ObjSTNative/STNativeCompiler.m
@@ -879,26 +879,6 @@ objectAccessor(MPWMachOClassWriter*, classwriter, setClasswriter)
 
 @implementation STNativeCompiler(testing) 
 
-+(void)testCompileSimpleClassAndMethod
-{
-    STNativeCompiler *compiler = [self compiler];
-    STClassDefinition * compiledClass = [compiler compile:@" class TestClass : NSObject {  -<int>hashPlus200  { self hash + 200. }}"];
-    IDEXPECT( compiledClass.name, @"TestClass", @"top level result");
-    INTEXPECT( compiledClass.methods.count,1,@"method count");
-    INTEXPECT( compiledClass.classMethods.count,0,@"class method count");
-    NSData *macho=[compiler compileClassToMachoO:compiledClass];
-    [macho writeToFile:@"/tmp/testclass-from-source.o" atomically:YES];
-    MPWMachOReader *reader = [MPWMachOReader readerWithData:macho];
-    EXPECTTRUE(reader.isHeaderValid, @"got a macho");
-    INTEXPECT([reader classReaders].count,1,@"number of classes" );
-    MPWMachOClassReader *classReader = [reader classReaders].firstObject;
-    IDEXPECT(classReader.nameOfClass, @"TestClass", @"name of class");
-    IDEXPECT(classReader.superclassPointer.targetName, @"_OBJC_CLASS_$_NSObject", @"symbol for superclass");
-    INTEXPECT(classReader.numberOfMethods, 1,@"number of methods");
-    IDEXPECT([classReader methodNameAt:0].targetPointer.stringValue,@"hashPlus200",@"name of method");
-    IDEXPECT([classReader methodTypesAt:0].targetPointer.stringValue,@"l@:",@"type of method");
-    IDEXPECT([classReader methodCodeAt:0].targetName,@"-[TestClass hashPlus200]",@"symbol for method code");
-}
 
 +(void)testCompileSimpleClassWithTwoMethods
 {
@@ -1303,7 +1283,6 @@ IDEXPECT(msg,@"No error",@"compile and run");\
 +(NSArray*)testSelectors
 {
    return @[
-       @"testCompileSimpleClassAndMethod",
        @"testCompileMethodWithMultipleArgs",
        @"testCompileSimpleClassWithTwoMethods",
        @"testJitCompileAMethod",
---
+++ ObjSTNative/STNativeCompilerTestsMachO.h
@@ -0,0 +1,16 @@
+//
+//  STNativeCompilerTestsMachO.h
+//  ObjSTNative
+//
+//  Created by Marcel Weiher on 11.05.24.
+//
+
+#import "STNativeCompiler.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface STNativeCompilerTestsMachO : STNativeCompiler
+
+@end
+
+NS_ASSUME_NONNULL_END
