3059146ef592d2e0ac602e1769acb81d4a302478
Marcel Weiher <marcel@metaobject.com>
2024-05-11 10:10:10 +0200

    separated out Mach-O specifc tests of ARMCodeGenerator.  Other tests, including the JIT stuff, pass on Linux

--- ObjSTNative/MPWARMObjectCodeGenerator.m
+++ ObjSTNative/MPWARMObjectCodeGenerator.m
@@ -376,8 +376,6 @@
 
 
 #import <MPWFoundation/DebugMacros.h>
-#import "MPWMachOWriter.h"
-#import "MPWMachOReader.h"
 
 @implementation MPWARMObjectCodeGenerator(testing) 
 
@@ -624,55 +622,6 @@ static void callme() {
     EXPECTTRUE(iWasCalled, @"called");
 }
 
-+(void)testGenerateMachOWithCallToExternalFunction
-{
-    MPWMachOWriter *writer = [MPWMachOWriter stream];
-    MPWARMObjectCodeGenerator *g=[self stream];
-    g.symbolWriter = writer;
-    g.relocationWriter = writer.textSectionWriter;
-    [g generateFunctionNamed:@"_theFunction" body:^(MPWARMObjectCodeGenerator *gen) {
-        [g generateCallToExternalFunctionNamed:@"_other"];
-    }];
-    [writer addTextSectionData:(NSData*)[g target]];
-//    NSLog(@"before write file");
-    [writer writeFile];
-//    NSLog(@"after write file");
-    NSData *macho=[writer data];
-    [macho writeToFile:@"/tmp/theFunction-calls-other.o" atomically:YES];
-    MPWMachOReader *reader=[[[MPWMachOReader alloc] initWithData:macho] autorelease];
-    INTEXPECT( [[reader textSection] offsetOfRelocEntryAt:0], 12,@"location of call to _other");
-    IDEXPECT( [[reader textSection] nameOfRelocEntryAt:0], @"_other",@"name of call to _other");
-}
-
-+(void)testGenerateMachOWithMessageSend
-{
-    MPWMachOWriter *writer = [MPWMachOWriter stream];
-    MPWARMObjectCodeGenerator *g=[self stream];
-    g.symbolWriter = writer;
-    g.relocationWriter = writer.textSectionWriter;
-    [g generateFunctionNamed:@"_lengthOfString" body:^(MPWARMObjectCodeGenerator *gen) {
-        [g generateMessageSendToSelector:@"length"];
-    }];
-    [writer addTextSectionData:(NSData*)[g target]];
-    [writer writeFile];
-    NSData *macho=[writer data];
-//    [macho writeToFile:@"/tmp/theFunction-sends-length.o" atomically:YES];
-    MPWMachOReader *reader=[[[MPWMachOReader alloc] initWithData:macho] autorelease];
-    INTEXPECT( [[reader textSection] offsetOfRelocEntryAt:0], 12,@"location of call to _other");
-    IDEXPECT( [[reader textSection] nameOfRelocEntryAt:0], @"_objc_msgSend$length",@"name of msg send");
-}
-
-+(void)testGenerateMessageSendAndComputation
-{
-    MPWARMObjectCodeGenerator *g=[self stream];
-    [g generateFunctionNamed:@"_lengthOfString" body:^(MPWARMObjectCodeGenerator *gen) {
-        [g generateMessageSendToSelector:@"hash"];
-        [gen generateAddDest:0 source:0 immediate:200];
-    }];
-    MPWJittableData *d=[g generatedCode];
-    NSData *code=[NSData dataWithBytes:d.bytes length:d.length];
-    [code writeToFile:@"/tmp/hashPlus200.code" atomically:YES];
-}
 
 typedef long (*IDPTR)(id,SEL);
 
@@ -776,9 +725,6 @@ typedef long (*IDIDPTR)(id,SEL,id);
        @"testGenerateBranchToPointerPassedAsArg",
        @"testGenerateCallToPointerPassedAsArg",
        @"testCanGenerateReturnAddressProtection",
-       @"testGenerateMachOWithCallToExternalFunction",
-       @"testGenerateMachOWithMessageSend",
-       @"testGenerateMessageSendAndComputation",
 //       @"testJITMessageSendAndComputation",
 //       @"testJITLengthOfPassedStringPlus3",
        @"testADRPGeneration",
