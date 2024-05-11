7a73aa9794d44c971e82da9e5c7b4529777eb8a3
Marcel Weiher <marcel@metaobject.com>
2024-05-11 14:23:40 +0200

    moved some more Mach-O specific tests

--- ObjSTNative/STNativeCompilerTestsMachO.m
+++ ObjSTNative/STNativeCompilerTestsMachO.m
@@ -42,10 +42,44 @@
     IDEXPECT([classReader methodCodeAt:0].targetName,@"-[TestClass hashPlus200]",@"symbol for method code");
 }
 
++(void)testCompileSimpleClassWithTwoMethods
+{
+    STNativeCompiler *compiler = [self compiler];
+    STClassDefinition * compiledClass = [compiler compile:@" class TestClass2Methods : NSObject {  -<int>hashPlus100 { self hash + 100. } -<int>hashPlus200  { self hash + 200. }}"];
+    IDEXPECT( compiledClass.name, @"TestClass2Methods", @"top level result");
+    INTEXPECT( compiledClass.methods.count,2,@"method count");
+    INTEXPECT( compiledClass.classMethods.count,0,@"class method count");
+    NSData *macho=[compiler compileClassToMachoO:compiledClass];
+    [macho writeToFile:@"/tmp/testclass2methods-from-source.o" atomically:YES];
+    MPWMachOReader *reader = [MPWMachOReader readerWithData:macho];
+    EXPECTTRUE(reader.isHeaderValid, @"got a macho");
+    INTEXPECT([reader classReaders].count,1,@"number of classes" );
+    MPWMachOClassReader *classReader = [reader classReaders].firstObject;
+    IDEXPECT(classReader.nameOfClass, @"TestClass2Methods", @"name of class");
+    IDEXPECT(classReader.superclassPointer.targetName, @"_OBJC_CLASS_$_NSObject", @"symbol for superclass");
+    INTEXPECT(classReader.numberOfMethods, 2,@"number of methods");
+    IDEXPECT([classReader methodNameAt:0].targetPointer.stringValue,@"hashPlus100",@"name of method");
+    IDEXPECT([classReader methodNameAt:1].targetPointer.stringValue,@"hashPlus200",@"name of method");
+    IDEXPECT([classReader methodTypesAt:0].targetPointer.stringValue,@"l@:",@"type of method");
+    IDEXPECT([classReader methodCodeAt:0].targetName,@"-[TestClass2Methods hashPlus100]",@"symbol for method code");
+    IDEXPECT([classReader methodCodeAt:1].targetName,@"-[TestClass2Methods hashPlus200]",@"symbol for method code");
+}
+
++(void)testCompileMethodWithMultipleArgs
+{
+    STNativeCompiler *compiler = [self compiler];
+    STClassDefinition * compiledClass = [compiler compile:@"class Concatter { -concat:a and:b { a, b. }}"];
+    IDEXPECT( compiledClass.name, @"Concatter", @"top level result");
+    INTEXPECT( compiledClass.methods.count,1,@"method count");
+    [[compiler compileClassToMachoO:compiledClass] writeToFile:@"/tmp/concatter.o" atomically:YES];
+}
+
 +(NSArray*)testSelectors
 {
    return @[
        @"testCompileSimpleClassAndMethod",
+       @"testCompileSimpleClassWithTwoMethods",
+       @"testCompileMethodWithMultipleArgs",
 			];
 }
 
