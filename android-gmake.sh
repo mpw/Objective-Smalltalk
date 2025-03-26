9d7069a31a407d0659907eb6766579ed509f591e
Marcel Weiher <marcel@metaobject.com>
2025-03-26 19:05:27 +0100

    built objst for Android/ARM, no tests

---
+++ android-gmake.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+source ~/Library/Android/GNUstep/arm64-v8a/share/GNUstep/Makefiles/GNUstep.sh
+export SDK=/Users/marcel/programming/Examples/Android-from-Scratch/android_sdk
+export CC="$SDK/ndk/26.2.11394342/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android33-clang"
+
+
+
+make $1
