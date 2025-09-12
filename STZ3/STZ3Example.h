f7328cbe27e5b76681702277c3f1ae00a0269ace
Marcel Weiher <marcel@metaobject.com>
2025-09-12 19:15:42 +0200

    start of optional Z3 support

---
+++ STZ3/STZ3Example.h
@@ -0,0 +1,16 @@
+//
+//  STZ3Example.h
+//  STZ3
+//
+//  Created by Marcel Weiher on 12.09.25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface STZ3Example : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
